e306f113a9d773093a7d4c81f96c1249ea3e180b
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2022-09-09 19:33:58 +0300

    HelloWorld now able to print custom string after "Hello World!"

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,8 +4,11 @@
  */
 // назавние главного класса, обычно совпадает с названием программы
 public class HelloWorld {
-  public static void main (String args[]) {
-    String helloMessage = "Hello World!";
-    System.out.println(helloMessage);
-  }
+    public static void main (String[] args) {
+        String helloMessage = "Hello World!";
+        System.out.println(helloMessage);
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        }
+    }
 }
