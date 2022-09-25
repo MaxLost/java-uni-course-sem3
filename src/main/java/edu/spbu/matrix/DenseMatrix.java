d59e9a95dda0e7fa27399bf187e77176036c7351
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2022-09-25 21:18:43 +0300

    Added dense matrix constructor from .txt file and method for getting an element from dense matrix

--- src/main/java/edu/spbu/matrix/DenseMatrix.java
+++ src/main/java/edu/spbu/matrix/DenseMatrix.java
@@ -1,47 +1,89 @@
 package edu.spbu.matrix;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 /**
  * Плотная матрица
  */
 public class DenseMatrix implements Matrix
 {
-  /**
-   * загружает матрицу из файла
-   * @param fileName
-   */
-  public DenseMatrix(String fileName) {
+	private final double[][] matrix;
+	private final int row_count;
+	private final int col_count;
+	/**
+	 * Loads dense matrix from file
+	 * @param fileName - name of file with matrix data
+	 */
+	public DenseMatrix(String fileName) {
+		Path file = Paths.get("src\\resources\\" + fileName);
+		try ( Scanner scanner = new Scanner(file) ) {
 
-  }
-  /**
-   * однопоточное умнджение матриц
-   * должно поддерживаться для всех 4-х вариантов
-   *
-   * @param o
-   * @return
-   */
-  @Override public Matrix mul(Matrix o)
-  {
-    return null;
-  }
+			int row_count = 0;
+			try ( Scanner counter = new Scanner(file) ) {
+				String line = "";
+				while (counter.hasNextLine()) {
+					row_count++;
+					line = counter.nextLine();
+				}
+				this.row_count = row_count;
+				this.col_count = line.split(" ").length;
+			}
 
-  /**
-   * многопоточное умножение матриц
-   *
-   * @param o
-   * @return
-   */
-  @Override public Matrix dmul(Matrix o)
-  {
-    return null;
-  }
+			this.matrix = new double[this.row_count][this.col_count];
 
-  /**
-   * спавнивает с обоими вариантами
-   * @param o
-   * @return
-   */
-  @Override public boolean equals(Object o) {
-    return false;
-  }
+			for (int i = 0; i < this.row_count; i++) {
+				for (int j = 0; j < this.col_count; j++) {
+					this.matrix[i][j] = Double.parseDouble(scanner.next());
+				}
+			}
+
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot open file", e);
+		}
+	}
+
+	public double getElement(int x, int y) {
+		if (y >= row_count | x >= col_count) {
+			throw new RuntimeException("Invalid coordinates");
+		}
+		else {
+			return matrix[x][y];
+		}
+	}
+
+	/**
+	 * однопоточное умнджение матриц
+	 * должно поддерживаться для всех 4-х вариантов
+	 *
+	 * @param o
+	 * @return
+	 */
+	@Override public Matrix mul(Matrix o)
+	{
+		return null;
+	}
+
+	/**
+	 * многопоточное умножение матриц
+	 *
+	 * @param o
+	 * @return
+	 */
+	@Override public Matrix dmul(Matrix o)
+	{
+		return null;
+	}
+
+	/**
+	 * спавнивает с обоими вариантами
+	 * @param o
+	 * @return
+	 */
+	@Override public boolean equals(Object o) {
+		return false;
+	}
 
 }
--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -6,14 +6,26 @@ import static org.junit.Assert.assertEquals;
 
 public class MatrixTest
 {
-  /**
-   * ожидается 4 таких теста
-   */
-  @Test
-  public void mulDD() {
-    Matrix m1 = new DenseMatrix("m1.txt");
-    Matrix m2 = new DenseMatrix("m2.txt");
-    Matrix expected = new DenseMatrix("result.txt");
-    assertEquals(expected, m1.mul(m2));
-  }
+	/**
+	 * ожидается 4 таких теста
+	 */
+
+	@Test
+	public void loadDenseMatrixTest() {
+		DenseMatrix m = new DenseMatrix("load_test.txt");
+		double[][] expected = { {1.0, 2.0, 0}, {3.0, 4.0, 1.0}, {10.0, 8.0, 0} };
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++){
+				assertEquals(expected[i][j], m.getElement(i, j), 0);
+			}
+		}
+	}
+
+	@Test
+	public void mulDD() {
+		Matrix m1 = new DenseMatrix("m1.txt");
+		Matrix m2 = new DenseMatrix("m2.txt");
+		Matrix expected = new DenseMatrix("result.txt");
+		assertEquals(expected, m1.mul(m2));
+	}
 }
